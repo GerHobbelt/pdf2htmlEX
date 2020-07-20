531f86666a8a8cee12e678472b379651c903261d
Janis Steinmann <janis.steinmann@amana.de>
2020-07-20 10:44:43 +0200

    - moved dotnet project - added docker support - fixed bug with options

--- DotNet.Converter/AMANA.PDF.Converter/PdfConverter.cs
+++ DotNet.Converter/AMANA.PDF.Converter/PdfConverter.cs
@@ -15,39 +15,37 @@ namespace AMANA.PDF.Converter
         public string ProcessOutput => _processOutput.ToString();
 
         public Dictionary<string, string> Pdf2HtmlExOptions = new Dictionary<string, string>();
-        
+
         public ZoomRatio ZoomRatio { get; set; }
         public FontFormat FontFormat { get; set; }
         public UnicodeHandling UnicodeHandling { get; set; }
         public AutoHintOptions AutoHintOptions { get; set; }
+        public string DockerMountPoint { get; set; }
 
         public PdfConverter(string pdfToHtmlExLocation)
         {
             _pdfToHtmlExLocation = pdfToHtmlExLocation;
             _pdfToHtmlExPath = Path.Combine(_pdfToHtmlExLocation, "pdf2htmlEX.exe");
+            DockerMountPoint = "pdf";
         }
 
-        public string Convert(string sourceFilePath, string targetFilePath = null)
+        private string Convert(ProcessStartInfo processStartInfo, string sourceFilePath, string targetFilePath = null)
         {
             string sourceFile = Path.Combine(_pdfToHtmlExLocation, "input.pdf");
             string destFile = Path.Combine(_pdfToHtmlExLocation, "input.html");
             File.Copy(sourceFilePath, sourceFile, true);
 
+            processStartInfo.WorkingDirectory = _pdfToHtmlExLocation;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.RedirectStandardInput = false;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.RedirectStandardError = true;
+
             Process pdf2HtmlEx = new Process
             {
-                StartInfo =
-                {
-                    WorkingDirectory = _pdfToHtmlExLocation,
-                    FileName = _pdfToHtmlExPath,
-                    Arguments =
-                        $"--no-drm 1 --embed-javascript 0 {ZoomRatio} {FontFormat} --decompose-ligature 1 --tounicode {UnicodeHandling} {AutoHintOptions} {GetOtherOptions()} input.pdf",
-                    UseShellExecute = false,
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    CreateNoWindow = true,
-                    RedirectStandardInput = false,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true
-                }
+                StartInfo = processStartInfo
             };
 
             _processOutput = new StringBuilder();
@@ -85,6 +83,30 @@ namespace AMANA.PDF.Converter
             return destFile;
         }
 
+        public string Convert(string sourceFilePath, string targetFilePath = null)
+        {
+            return Convert(
+                new ProcessStartInfo()
+                {
+                    FileName = _pdfToHtmlExPath,
+                    Arguments = $"--no-drm 1 {GetZoomOption()} {GetFontFormatOption()} --decompose-ligature 1 --tounicode {GetUnicodeHandlingOption()} {GetAutoHintOption()} {GetOtherOptions()} input.pdf",
+                },
+                sourceFilePath,
+                targetFilePath);
+        }
+
+        public string DockerConvert(string containerName, string sourceFilePath, string targetFilePath = null)
+        {
+            return Convert(
+                new ProcessStartInfo()
+                {
+                    FileName = "docker",
+                    Arguments = $"run --rm -it -v {GetVolumeString()} {containerName} --no-drm 1 {GetZoomOption()} {GetFontFormatOption()} --decompose-ligature 1 --tounicode {GetUnicodeHandlingOption()} {GetAutoHintOption()} {GetOtherOptions()} input.pdf",
+                },
+                sourceFilePath,
+                targetFilePath);
+        }
+
         private void ProcessOutputHandler(object sendingProcess,
             DataReceivedEventArgs outLine)
         {
@@ -92,6 +114,75 @@ namespace AMANA.PDF.Converter
             {
                 _processOutput.Append(Environment.NewLine);
                 _processOutput.Append(outLine.Data);
+            }
+        }
+
+        private string GetVolumeString()
+        {
+            return $"{_pdfToHtmlExLocation.Replace("\\", "/")}:/{DockerMountPoint}";
+        }
+
+        private string GetAutoHintOption()
+        {
+            if (AutoHintOptions == AutoHintOptions.UseAutoHint)
+                return "--auto-hint 1";
+            return "";
+        }
+
+        private string GetZoomOption()
+        {
+            switch (ZoomRatio)
+            {
+                case ZoomRatio.Ratio1:
+                    return "--zoom 1.1";
+
+                case ZoomRatio.Ratio2:
+                    return "--zoom 1.3";
+
+                case ZoomRatio.Ratio3:
+                    return "--zoom 1.5";
+
+                case ZoomRatio.Ratio4:
+                    return "--zoom 1.75";
+
+                case ZoomRatio.Ratio5:
+                    return "--zoom 2";
+
+                case ZoomRatio.Ratio6:
+                    return "--zoom 2.5";
+
+                case ZoomRatio.Ratio7:
+                    return "--zoom 3";
+
+                default:
+                    return "";
+            }
+        }
+
+        private string GetFontFormatOption()
+        {
+            switch (FontFormat)
+            {
+                case FontFormat.OTF:
+                    return "--font-format otf";
+                case FontFormat.TTF:
+                    return "--font-format ttf";
+                default:
+                    return "";
+            }
+        }
+
+        private string GetUnicodeHandlingOption()
+        {
+            switch (UnicodeHandling)
+            {
+                case UnicodeHandling.Force:
+                    return "1";
+                case UnicodeHandling.Ignore:
+                    return "-1";
+                default:
+                case UnicodeHandling.Auto:
+                    return "0";
             }
         }
 
@@ -114,7 +205,6 @@ namespace AMANA.PDF.Converter
                     case "embed-css":
                     case "embed-font":
                     case "embed-image":
-                    case "embed-javascript":
                     case "tmp-dir":
                     case "data-dir":
                         ignore = true;
